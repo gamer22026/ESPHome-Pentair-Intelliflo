d1bc89269495c089c7d78279418c13727668aaa5
Kevin McDermott <gamer22026@gmail.com>
2026-07-26 14:33:18 -0700

    Add files via upload

--- components/pentair_intelliflo/intelliflo.h
+++ components/pentair_intelliflo/intelliflo.h
@@ -1,14 +1,12 @@
 #pragma once
 
+#include "esphome/core/component.h"
 #include "esphome/components/binary_sensor/binary_sensor.h"
 #include "esphome/components/sensor/sensor.h"
-//#include "esphome/components/select/select.h"
-// #include "esphome/components/switch/switch.h"
 #include "esphome/components/text_sensor/text_sensor.h"
 #include "esphome/components/uart/uart.h"
-// #include "esphome/core/automation.h"
-#include "esphome/core/component.h"
 #include <queue>
+#include <vector>
 
 namespace esphome {
 namespace intelliflo {
@@ -35,55 +33,18 @@ enum program : uint8_t {
 };
 
 class Intelliflo : public uart::UARTDevice, public PollingComponent {
-  void switch_command(const std::string &command);
+ public:
   void setup() override;
   void loop() override;
   void dump_config() override;
   void update() override;
 
- protected:
-  uint32_t last_received_byte_millis = 0;
-
-  void send_array_cmd(std::vector<uint8_t> data);
-  void send_array_cmd(const uint8_t *data, size_t len);
-
-  void parse_packet(const std::vector<uint8_t> &data);  // parsing the status package
-
-  void handle_received_byte(uint8_t c);  // received byte handler
-  bool validate_received_message();      // function of checking the received message
-
-  void query_status();
-
-  void publish_state_if_changed();
-
-  std::vector<uint8_t> rx_buffer;
-  std::queue<std::vector<uint8_t>> tx_buffer;
-  bool ready_to_tx = true;
-
-  void QueuePacket(uint8_t message[], int messageLength);
-
-  sensor::Sensor *power_;
-  sensor::Sensor *rpm_;
-  sensor::Sensor *flow_;
-  sensor::Sensor *pressure_;
-
-  binary_sensor::BinarySensor *running_;
-
-  text_sensor::TextSensor *program_;
-
-  //select::Select *pumpmode_selector_{nullptr};
-
- public:
   void set_power(sensor::Sensor *sensor) { power_ = sensor; }
   void set_rpm(sensor::Sensor *sensor) { rpm_ = sensor; }
   void set_flow(sensor::Sensor *sensor) { flow_ = sensor; }
   void set_pressure(sensor::Sensor *sensor) { pressure_ = sensor; }
-
   void set_running(binary_sensor::BinarySensor *sensor) { running_ = sensor; }
-
   void set_program(text_sensor::TextSensor *sensor) { program_ = sensor; }
-
-  //void void set_operating_mode_select(select::Select *selector) { pumpmode_selector_ = selector; };
 
   void requestPumpStatus();
   void run();
@@ -92,10 +53,29 @@ class Intelliflo : public uart::UARTDevice, public PollingComponent {
   void commandExternalProgram(int prog);
   void saveValueForProgram(int prog, int value);
   void commandRPM(int rpm);
-  void commandFlow(int flow);  // In m3/H * 10
+  void commandFlow(int flow);
   void pumpToLocalControl();
   void pumpToRemoteControl();
 
+ protected:
+  uint32_t last_tx_millis_{0};
+  uint32_t last_received_byte_millis{0};
+
+  void send_array_cmd(const uint8_t *data, size_t len);
+  void parse_packet(const std::vector<uint8_t> &data);
+  void handle_received_byte(uint8_t c);
+  bool validate_received_message();
+  void QueuePacket(uint8_t message[], int messageLength);
+
+  std::vector<uint8_t> rx_buffer;
+  std::queue<std::vector<uint8_t>> tx_buffer;
+
+  sensor::Sensor *power_{nullptr};
+  sensor::Sensor *rpm_{nullptr};
+  sensor::Sensor *flow_{nullptr};
+  sensor::Sensor *pressure_{nullptr};
+  binary_sensor::BinarySensor *running_{nullptr};
+  text_sensor::TextSensor *program_{nullptr};
 };
 
 }  // namespace intelliflo
